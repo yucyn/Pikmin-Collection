4ef82e9ed74ed0f70d92ab7282e34b84a55e111f
yucyn <cliu6481@gmail.com>
2026-04-28 23:30:34 +0800

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -2,12 +2,12 @@
 // 請只修改這個檔案，把 Firebase Console 的 Web App config 貼進來。
 
 window.PIKMIN_FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
+  apiKey: "AIzaSyAZDSSH1IPKg5f0H59Rs7cuI_UfgpHfZ7E",
   authDomain: "pikmin-collection.firebaseapp.com",
   projectId: "pikmin-collection",
   storageBucket: "pikmin-collection.firebasestorage.app",
   messagingSenderId: "727063039400",
-  appId: "PASTE_YOUR_APP_ID"
+  appId: "1:727063039400:web:10ca96f30693d9c671e19d"
 };
 
 window.PIKMIN_FIREBASE_COLLECTION = "pikmin_postcards";

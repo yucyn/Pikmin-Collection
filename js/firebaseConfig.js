46e0cfbe71c77f2aeb0eeb93adf71211cdf27c38
yucyn <cliu6481@gmail.com>
2026-04-28 18:34:16 +0800

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -4,12 +4,11 @@
 // 3. 沒填設定時，系統會自動退回 localStorage 測試模式
 
 window.PIKMIN_FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyAZDSSH1IPKg5f0H59Rs7cuI_UfgpHFz7E",
+  authDomain: "pikmin-collection.firebaseapp.com",
+  projectId: "pikmin-collection",
+  storageBucket: "pikmin-collection.firebasestorage.app",
+  messagingSenderId: "727063039400",
+  appId: "1:727063039400:web:10ca96f30693d9c671e19d"
 };
-
 window.PIKMIN_FIREBASE_COLLECTION = "pikmin_postcards";

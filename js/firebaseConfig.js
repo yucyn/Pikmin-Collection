3067b7d324cfa0e1c2a7f719c662afebb33d500d
yucyn <cliu6481@gmail.com>
2026-04-28 23:29:35 +0800

    Add files via upload

---
+++ js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Firebase 設定檔
+// 請只修改這個檔案，把 Firebase Console 的 Web App config 貼進來。
+
+window.PIKMIN_FIREBASE_CONFIG = {
+  apiKey: "PASTE_YOUR_API_KEY",
+  authDomain: "pikmin-collection.firebaseapp.com",
+  projectId: "pikmin-collection",
+  storageBucket: "pikmin-collection.firebasestorage.app",
+  messagingSenderId: "727063039400",
+  appId: "PASTE_YOUR_APP_ID"
+};
+
+window.PIKMIN_FIREBASE_COLLECTION = "pikmin_postcards";
